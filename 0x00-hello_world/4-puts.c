756ec3106e8f7955358ce1f5d375c6d5768b4e10
Franky <frankochari@gmail.com>
2022-12-08 05:14:48 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main -print using puts function
  *
  * Return:0
@@ -7,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
