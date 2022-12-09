78da1c93dbccdfb83fe22dcfb8a4c451f4f9d9ec
Franky <frankochari@gmail.com>
2022-12-09 09:27:42 -0800

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - alpahabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char al;
+
+	for (al = 'z'; al >= 'a'; al++)
+		putchar(al);
+	putchar('\n');
+	return (0);
+}
