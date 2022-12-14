9a2e17445ed9898b35747787308b73f692ccdb76
Franky <frankochari@gmail.com>
2022-12-14 10:42:02 -0800

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Entry for the addtion
+ * @a: value to added together
+ * @b: second value to be added
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
