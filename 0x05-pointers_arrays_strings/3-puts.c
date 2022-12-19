52f66f56d6e5e201668e9793d0849d52371ef0e2
Franky <frankochari@gmail.com>
2022-12-19 13:15:23 -0800

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ * @str: a pramater to input function
+ * Return: Always 0
+ */
+void _puts(char *str);
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
