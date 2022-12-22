214c376f5d71e13108a28b39f80e6fc9dbb5bfcb
Franky <frankochari@gmail.com>
2022-12-22 12:30:13 -0800

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @v: string to be capitalized
+ *
+ * Return: pointer to the change string
+ */
+char *cap_string(char *v)
+{
+	char cps[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}' };
+	int len = 13;
+	int a = 0, i;
+
+	while (v[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || v[a - 1] == cps[i] && (v[a] >= 97 && v[a] <= 122))
+					v[a] = v[a] - 32;
+			i++;
+		}
+		a++;
+	}
+	return (v);
+
+}
+
