97139595bda9e208b78bb891d3f8629d628e5541
Franky <frankochari@gmail.com>
2022-12-22 10:39:34 -0800

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -  compares two strings
+ * @s1: input 1
+ * @s2: input 2
+ * Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp;
+
+	comp = strcmp(s1, s2);
+	return (comp);
+}
