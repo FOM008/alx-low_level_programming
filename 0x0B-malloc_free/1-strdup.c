edccccd0ff91efd5f2c79501e2e6d266ff2252b7
Franky <frankochari@gmail.com>
2023-01-11 07:58:39 -0800

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,11 +17,15 @@ char *_strdup(char *str)
 
 	while (str[i] != '\0')
 		i++;
+
 	aaa = malloc(sizeof(char) * (i + 1))
 
 		if (aaa == NULL)
 			return (NULL);
+
 	for (r = 0; str[r]; r++)
 		aaa[r] = str[r];
+
+
 	return (aaa);
 }
