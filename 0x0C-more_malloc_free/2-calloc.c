8f3bfb6d97001ae06587dedadcf3a04f66ae4696
Franky <frankochari@gmail.com>
2023-01-14 01:51:36 -0800

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -26,7 +26,7 @@ char *_memset(char *s, char b, unsigned int n)
  *
  *  Return: pointer to the memory area s
  */
-void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
 
