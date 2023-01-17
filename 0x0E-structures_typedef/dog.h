6675e883abb6f1cbd7396dc8598333626f41082e
Franky <frankochari@gmail.com>
2023-01-17 08:38:40 -0800

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,8 @@ struct dog
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
